83989c297d7977a15cfa73d4c29f8cab28a81051
boris.sizov.2001 <boris.sizov.2001@mail.ru>
2020-09-26 17:14:47 +0300

    Was added animations and attached properties to work with them.

---
+++ Cargo_Transportation/Animations/PageAnimation.cs
@@ -0,0 +1,78 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace Cargo_Transportation.Animations
+{
+    public enum PageAnimation
+    {
+        None = 0,
+        SlideAndFadeInFromRight = 1,
+        SlideAndFadeOutToLeft = 2,
+    }
+
+    public static class PageAnimations
+    {
+        public static async Task SlideAndFadeInFromRight(this FrameworkElement element, float seconds = 0.3f, bool keepMargin = true)
+        {
+            var sb = new Storyboard();
+
+            sb.AddSlideFromRight(seconds, element.ActualWidth, keepMargin: keepMargin);
+            sb.AddFadeIn(seconds);
+            sb.Begin(element);
+            element.Visibility = Visibility.Visible;
+            await Task.Delay((int)(seconds * 1000));
+        }
+        public static async Task SlideAndFadeInFromLeft(this FrameworkElement element, float seconds = 0.3f)
+        {
+            var sb = new Storyboard();
+
+            sb.AddSlideFromLeft(seconds, element.ActualWidth);
+            sb.AddFadeIn(seconds);
+            sb.Begin(element);
+            element.Visibility = Visibility.Visible;
+            await Task.Delay((int)(seconds * 1000));
+        }
+        public static async Task SlideAndFadeOutToLeft(this FrameworkElement element, float seconds = 0.3f)
+        {
+            var sb = new Storyboard();
+
+            sb.AddSlideToLeft(seconds, element.ActualWidth);
+            sb.AddFadeOut(seconds);
+            sb.Begin(element);
+            element.Visibility = Visibility.Visible;
+            await Task.Delay((int)(seconds * 1000));
+        }
+        public static async Task SlideAndFadeOutToRight(this FrameworkElement element, float seconds = 0.3f)
+        {
+            var sb = new Storyboard();
+
+            sb.AddSlideToRight(seconds, element.ActualWidth);
+            sb.AddFadeOut(seconds);
+            sb.Begin(element);
+            element.Visibility = Visibility.Visible;
+            await Task.Delay((int)(seconds * 1000));
+        }
+        public static async Task FadeIn(this FrameworkElement element, float seconds = 0.3f)
+        {
+            var sb = new Storyboard();
+
+            sb.AddFadeIn(seconds);
+            sb.Begin(element);
+            if (seconds != 0)
+                element.Visibility = Visibility.Visible;
+            await Task.Delay((int)(seconds * 1000));
+        }
+        public static async Task FadeOut(this FrameworkElement element, float seconds = 0.3f)
+        {
+            var sb = new Storyboard();
+
+            sb.AddFadeOut(seconds);
+            sb.Begin(element);
+            if (seconds != 0)
+                element.Visibility = Visibility.Visible;
+            await Task.Delay((int)(seconds * 1000));
+            element.Visibility = Visibility.Collapsed;
+        }
+    }
+}
---
+++ Cargo_Transportation/AttachedProperties/AnimationsAttachedProperties.cs
@@ -0,0 +1,65 @@
+﻿using Cargo_Transportation.Animations;
+using System.Windows;
+
+namespace Cargo_Transportation.AttachedProperties
+{
+    public abstract class AnimateBaseProperty<Parent> : BaseAttachedProperty<Parent, bool>
+           where Parent : BaseAttachedProperty<Parent, bool>, new()
+    {
+        public bool FirstLoad { get; set; } = true;
+
+        public override void OnValueUpdated(DependencyObject sender, object value)
+        {
+            if (!(sender is FrameworkElement element))
+                return;
+            if (sender.GetValue(ValueProperty) == value && !FirstLoad)
+                return;
+            if (FirstLoad)
+            {
+                RoutedEventHandler onLoaded = null;
+                onLoaded = (ss, ee) =>
+                {
+                    element.Loaded -= onLoaded;
+                    DoAnimation(element, (bool)value);
+                    FirstLoad = false;
+                };
+                element.Loaded += onLoaded;
+            }
+            else
+                DoAnimation(element, (bool)value);
+        }
+
+        protected virtual void DoAnimation(FrameworkElement element, bool value) { }
+    }
+
+    public class AnimateSlideInFromLeftProperty : AnimateBaseProperty<AnimateSlideInFromLeftProperty>
+    {
+        protected override async void DoAnimation(FrameworkElement element, bool value)
+        {
+            if (value)
+                await element.SlideAndFadeInFromLeft(FirstLoad ? 0 : 0.7f);
+            else
+                await element.SlideAndFadeOutToLeft(FirstLoad ? 0 : 0.7f);
+        }
+    }
+    public class AnimateSlideInFromRightProperty : AnimateBaseProperty<AnimateSlideInFromRightProperty>
+    {
+        protected override async void DoAnimation(FrameworkElement element, bool value)
+        {
+            if (value)
+                await element.SlideAndFadeInFromRight(FirstLoad ? 0 : 0.7f);
+            else
+                await element.SlideAndFadeOutToRight(FirstLoad ? 0 : 0.7f);
+        }
+    }
+    public class AnimateFadeInProperty : AnimateBaseProperty<AnimateFadeInProperty>
+    {
+        protected override async void DoAnimation(FrameworkElement element, bool value)
+        {
+            if (value)
+                await element.FadeIn(FirstLoad ? 0 : 0.3f);
+            else
+                await element.FadeOut(FirstLoad ? 0 : 0.3f);
+        }
+    }
+}
