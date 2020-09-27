0b8d99e916d1eab3c87c9b2e9449419af8060e82
boris.sizov.2001 <boris.sizov.2001@mail.ru>
2020-09-27 13:25:36 +0300

    Was added login and register pages? and page host for subsequent implementation.

---
+++ Cargo_Transportation/AttachedProperties/NoFrameHistory.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Cargo_Transportation.AttachedProperties
+{
+    public class NoFrameHistory : BaseAttachedProperty<NoFrameHistory, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var frame = (sender as Frame);
+
+            frame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            frame.Navigated += (ss, ee) => ((Frame)ss).NavigationService.RemoveBackEntry();
+        }
+    }
+}
---
+++ Cargo_Transportation/ValueConverters/BaseValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Cargo_Transportation.ValueConverters
+{
+    public abstract class BaseValueConverter<T> : MarkupExtension, IValueConverter
+        where T : class, new()
+    {
+        private static T _converter = null;
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => _converter ??= new T();
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
